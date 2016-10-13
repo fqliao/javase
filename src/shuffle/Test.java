620a6564ee26ce1ba47311315417e19793a041ff
fqliao <fqliao@yeah.net>
2016-10-13 20:18:08 +0800

    shuffle+sort

---
+++ src/shuffle/Test.java
@@ -0,0 +1,33 @@
+package shuffle;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+/**
+ * @decription:The program demonstrates the random shuffle and sort algorithms.
+ * @version:1.0
+ * @date: 2016年10月13日下午8:10:32
+ * @author: lfq
+ * 
+ */
+public class Test {
+
+	public static void main(String[] args) {
+		List<Integer> numbers = new ArrayList<>();
+		for (int i = 0; i <= 49; i++) {
+			numbers.add(i);
+		}
+		//打乱元素顺序
+		Collections.shuffle(numbers);
+		//获取前五个元素
+		List<Integer> subList = numbers.subList(0, 6);
+		//对取出的元素排序
+		Collections.sort(subList);
+		
+		System.out.println(subList);
+		
+	}
+
+}
